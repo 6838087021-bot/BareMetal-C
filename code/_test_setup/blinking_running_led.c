0eed249eaf7d4987ff8302325ff9c7c169470ec8
kongkrit <45794368+kongkrit@users.noreply.github.com>
2025-12-26 17:50:37 +0700

    add test for setup validation

---
+++ code/_test_setup/blinking_running_led.c
@@ -0,0 +1,35 @@
+#include <stdbool.h>
+#include <stdint.h>
+
+#define LED0 ((volatile bool *)0xE000U)
+
+#define COUNTER ((volatile uint16_t *)0x40F0U)
+
+void delay(uint16_t n) {
+    uint16_t *c = COUNTER;
+    *c = 0;
+    while (*c < n) {
+        *c = *c + 1;
+    }
+}
+
+void main(void) {
+
+    volatile bool *led;
+
+    uint16_t on  = 1500;
+    uint16_t off = 1000;
+
+    while (true) {
+        for (uint8_t i = 0; i < 4; i = i + 1) {
+            led = LED0 + i;
+            *led = true;
+            delay(on);
+            *led = false;
+            delay(off);
+            *led = true;
+            delay(on);
+            *led = false;
+        }
+    }
+}
