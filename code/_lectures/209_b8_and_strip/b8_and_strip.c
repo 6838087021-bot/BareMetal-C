fe6b08715bda8538ff942b457a087720b70e9d1d
kongkrit <45794368+kongkrit@users.noreply.github.com>
2026-01-04 15:27:36 +0700

    Create b8_and_strip.c

---
+++ code/_lectures/209_b8_and_strip/b8_and_strip.c
@@ -0,0 +1,20 @@
+// 06-209
+#include <stdlib.h>
+#include <stdbool.h>
+#include "baremetal_binary.h" // for B8()
+#include "baremetal_delay.h"  // for baremetal_delay()
+
+#define LED_STRIP ((volatile uint8_t *)0xE010U)
+
+void main(void) {
+    volatile uint8_t *s = LED_STRIP;
+
+    while (true) {
+        *s = B8(10101111);     // X.X.XXXX
+        baremetal_delay(4000);
+        *s = B8(11110101);     // XXXX.X.X
+        baremetal_delay(4000);
+        *s = B8(01010101);     // .X.X.X.X
+        baremetal_delay(4000);
+    }
+}
